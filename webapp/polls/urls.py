add315549532589cd9ca1f5af3e3b283d94efa46
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-19 18:50:34 +0100

    handled Django html forms

--- webapp/polls/urls.py
+++ webapp/polls/urls.py
@@ -3,15 +3,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('main', TemplateView.as_view(template_name='polls/main.html')),
-    path('default', TemplateView.as_view(template_name='polls/default.html')),
-    path('', views.index, name='index'),
-    path('funky', views.funky),
-    path('danger', views.danger),
-    path('steam/<int:guess>', views.steam),
-    path('class1', views.ClassView1.as_view()),
-    path('class2', views.ClassView2.as_view()),
-    path('ClassView1/<int:try1>', views.ClassView3.as_view()),
-    path('ClassView2/<slug:try2>', views.ClassView3.as_view()),
-    path('bounce', views.BounceView.as_view()),
+    path('', TemplateView.as_view(template_name='polls/index.html'), name='index' ),
+    path('form', views.ClassView.as_view(), name='form'),
+
 ]
--- webapp/polls/views.py
+++ webapp/polls/views.py
@@ -2,82 +2,13 @@ from django.http import HttpResponse, response
 from django.http import HttpResponseRedirect
 from django.utils.html import escape
 from django.views import View
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse('''Hello, world. You're at the polls index.
-    It is great to have you here. This is one of my Django apps''')
-
-def funky(request):
-    response= '''
-    <h1> This is the funky webpage </h1>  
-    '''
-    return HttpResponse(response)
-
-def danger(request):
-    response='''
-    <html><body>
-    <h3>This is the danger page. Your guess is 
-    ''' + escape(request.GET['guess'])+'''</h3>
-    </body>
-    </html>  
-    '''
-    return HttpResponse(response)
-
-def steam(request, guess):
-    response='''
-    <html><body>
-    <h3>This is the danger page. Your guess is 
-    ''' + escape(guess)+'''</h3>
-    </body>
-    </html>  
-    '''
-    return HttpResponse(response)
+class ClassView(View):
+    def get(self, request):
+        return render(request, 'polls/form.html')
     
-class ClassView1(View):
-    def get(self, reqest):
-        response = '''
-        <html><body>
-        <h4>Welcome to the ClassView1 HTML Page</h4>
-        </body>
-        </html>
-        '''
-        return HttpResponse(response)
-
-
-class ClassView2(View):
-    def get(self, request):
-        response = '''
-        <html><body>
-        <h4>Welcome to the ClassView2 HTML Page</h4>
-        </body>
-        </html>
-        '''
-        return HttpResponse(response)
-
-
-class ClassView3(View):
-    def get(self, request, try1):
-        response='''
-    <html><body>
-    <h3>This is ClassView3 page. Your guess is 
-    ''' + escape(try1)+'''</h3>
-    </body>
-    </html>  
-    '''
-        return HttpResponse(response)
-
-
-class ClassView4(View):
-    def get(self, request, try2):
-        response='''
-        <html><body>
-        <h3>This is ClassView3 page. Your guess is 
-        ''' + escape(try2)+'''</h3>
-        </body>
-        </html>  
-        '''
-        return HttpResponse(response)
-
-class BounceView(View):
-    def get(self, request):
-        return HttpResponseRedirect('https://www.google.com')
+    def post(self, request):
+        response = request.POST.get('input')
+        context = {'input' : response}
+        return render(request, 'polls/form.html', context)
