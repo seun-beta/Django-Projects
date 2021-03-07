4616202af2e83df680cbb0cf47c8e3aba41e713c
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-07 20:03:03 +0100

    added new urls to path

--- webapp/polls/urls.py
+++ webapp/polls/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', TemplateView.as_view(template_name='polls/index.html'), name='index' ),
     path('form', views.ClassView.as_view(), name='form'),
+    path('dump', views.DumpPython.as_view(), name='dump'),
+    path('reverse', views.ReversePython.as_view(), name='reverse'),
+
 
 ]
