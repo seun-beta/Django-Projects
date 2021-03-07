a9d9eabe3bb6caae31935a22dc0ceb9c146db6b4
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-07 23:15:09 +0100

    made index a class view

--- webapp/polls/urls.py
+++ webapp/polls/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.views.generic import TemplateView
 from . import views
 
+app_name ='polls'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='polls/index.html'), name='index' ),
+    path('', views.Index.as_view(), name='index' ),
     path('form', views.ClassView.as_view(), name='form'),
     path('dump', views.DumpPython.as_view(), name='dump'),
     path('reverse', views.ReversePython.as_view(), name='reverse'),
-
 
 ]
