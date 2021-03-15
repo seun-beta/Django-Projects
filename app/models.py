bea94cf74cb60f2e8fc291d9800abc6ecaedf15e
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-15 22:05:49 +0100

    created model for the database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.conf import settings
 
-# Create your models here.
+
+class Advert(models.Model):
+    title = models.CharField(
+        max_length=256,
+        help_text="Input the title of your advert",
+        validators=[MinLengthValidator
+                    (1, "Please input a title with one or more characters")]
+    )
+    price = models.DecimalField(decimal_places=7, max_digits=10)
+    text = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
