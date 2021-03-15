b9d79795a45a107ad8d307f124692f206014d499
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-15 22:07:40 +0100

    created views for the various url routes

--- app/views.py
+++ app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from app.owner import OwnerCreateView, OwnerDeleteView, OwnerUpdateView
+from django.views import View
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+from app.models import Advert
+
+
+class AdvertListView(ListView):
+    model = Advert
+
+
+class FullAdvertListView(LoginRequiredMixin, ListView):
+    model = Advert
+
+
+class AdvertDetailView(LoginRequiredMixin, DetailView):
+    model = Advert
+
+
+class AdvertCreateView(OwnerCreateView):
+    model = Advert
+    fields = ['title', 'price', 'text']
+    success_url = reverse_lazy('app:all_ad')
+
+
+class AdvertUpdateView(OwnerUpdateView):
+    model = Advert
+    fields = ['title', 'price', 'text']
+    success_url = reverse_lazy('app:all_ad')
+
+
+class AdvertDeleteView(OwnerDeleteView):
+    model = Advert
+    success_url = reverse_lazy('app:all_ad')
